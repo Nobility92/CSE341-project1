8df5d580e7203ffc32763dbb093a7321742154fb
Nobility92 <nwa21004@byui.edu>
2024-07-11 13:23:43 +0100

    Authenticate

--- app.js
+++ app.js
@@ -2,9 +2,21 @@ const express = require('express');
 const mongodb = require('./data/database');
 const app = express();
 const bodyParser = require('body-parser');
+const passport = require('passport');
+const session = require('express-session');
+const githubstrategy = require('passport-github2').Strategy;
+const cors = require('cors');
 const port = 3000;
 
 app.use(bodyParser.json());
+app.use(session({
+  secret:"secret",
+  resave: false,
+  saveUninitialized: true,
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+
 app.use((req, res, next) =>{
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader(
@@ -15,13 +27,46 @@ app.use((req, res, next) =>{
     next();
 });
 
-app.use('/', require('./routes'));
+app.use(cors({methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH']}))
+app.use(cors({origin: '*'}))
 app.use('/', require('./routes/staff'));
+
+
+app.use('/', require('./routes'));
 app.use('/', require('./routes/users'));
 
 app.use('/', require('./routes/temples'));
 app.use('/', require('./routes/indexswag'));
 //app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+passport.use(new githubstrategy({
+  clientID: process.env.GITHUB_CLIENT_ID,
+  clientSecret: process.env.GITHUB_CLIENT_SECRET,
+  callbackURL: process.env.CALLBACK_URL
+  
+},
+  function(accessToken, refreshToken, profile, done){
+    return done (null, profile);
+  }
+));
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+passport.deserializeUser((user, done) =>{
+  done(null, user);
+});
+
+app.get('/', (req, res) => {
+  res.send(req.session.user !== undefined? `Logged In as ${req.session.user.displayName}` : `Logged Out`)
+});
+
+app.get('/github/callback', passport.authenticate('github', {
+  failureRedirect: '/staff', session: false}),
+(req, res) => {
+  req.session.user = req.user;
+  res.redirect('/staff');
+});
 
 mongodb.initDb((err) =>{
   if(err){
--- routes/staff.js
+++ routes/staff.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const routes = require('express').Router();
+const passport = require('passport');
 
 const staffcontroller = require("../controllers/staff");
 const validate = require("../middleware/validate");
+const {IsAuthenticated} = require("../middleware/authenticate");
 
 routes.get('/staff', staffcontroller.displayAll);
 routes.get('/:id', staffcontroller.displaySingle);
-routes.post('/staff', validate.saveUser, staffcontroller.createStaff);
-routes.put('/:id', validate.saveUser, staffcontroller.updateStaff);
-routes.delete('/:id', staffcontroller.deleteStaff);
+routes.post('/staff', IsAuthenticated, validate.saveUser, staffcontroller.createStaff);
+routes.put('/:id', IsAuthenticated, validate.saveUser, staffcontroller.updateStaff);
+routes.delete('/:id', IsAuthenticated, staffcontroller.deleteStaff);
+
+routes.get('/login', passport.authenticate('github'), (req, res) => {});
+
+routes.get('/logout', function(req, res, next) {
+    req.logout(function(err){
+        if (err){return next(err);}
+        res.redirect('/');
+    });
+});
 
 module.exports = routes
